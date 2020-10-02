3e06a70b72ea2333c1758f7a286afb4d37230ea5
Marcos-SCO <marcos_sco@outlook.com>
2020-10-02 09:02:38 -0300

    Connection

--- app/config/Conn.php
+++ app/config/Conn.php
@@ -2,17 +2,14 @@
 
 namespace App\Config;
 
-use App\Traits\QueryTrait;
 use App\Interfaces\DatabaseInterface;
 
 class Conn
 {
     public $conn;
 
-    use QueryTrait;
-
     public function __construct(DatabaseInterface $conn)
     {
-        $this->conn = $conn->pdo;
+        return $this->conn = $conn->pdo;
     }
 }
