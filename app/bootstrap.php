2cbf2a3018f774a1a4b86d6f5b6cdf6a2648ca10
Marcos-SCO <marcos_sco@outlook.com>
2020-10-02 12:32:08 -0300

    Bootstrap config

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -1,6 +1,12 @@
 <?php 
 
-require "./vendor/autoload.php";
+require_once "./vendor/autoload.php";
+require_once "./app/helpers/functions.php";
+
+use App\Config\Conn;
+use App\Config\MySql;
+use App\Models\Model;
+use App\Models\User;
 
 // Env variables will be in $_ENV array
 $dotenv = Dotenv\Dotenv::createImmutable(dirname(__DIR__, 1), '.env');
@@ -9,3 +15,14 @@ $dotenv->load();
 
 // Timezone
 date_default_timezone_set($_ENV["DEFAULT_TIME_ZONE"]);
+
+// Connection 
+$conn = new Conn(new MySql);
+
+// $c = new Model(new App\Config\MySql());
+
+// $model = new Model($conn);
+// $user = new User($conn);
+// var_dump($user->select('users'));
+
+require_once "./app/core/routes.php";
