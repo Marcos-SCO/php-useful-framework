ce8edff2c820311206e23a42591cb95f43fb6d44
Marcos-SCO <marcos_sco@outlook.com>
2020-10-02 12:33:12 -0300

    Router

--- index.php
+++ index.php
@@ -1,27 +1,8 @@
 <?php
 
-function dump($dump) {
-    echo '<pre style="font-size:1.2rem;">';
-        var_dump($dump);
-    echo '</pre>';
-    return;
-}
-
-use App\Config\Conn;
-use App\Config\MySql;
-use App\Models\Model;
-use App\Models\User;
-
 require_once "./app/bootstrap.php";
 
-// Connection 
-$conn = new Conn(new MySql);
-// $c = new Model(new App\Config\MySql());
-
-// $model = new Model($conn);
-$user = new User($conn);
-var_dump($user->select('users'));
-
+var_dump($_GET);
 
 //$select = $model->select('users');
 // var_dump($select);
